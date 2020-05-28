ef05b3b85e7d0e23acf5a5a53e4393d3db3ea76a
Marcelle Pernet <mevpernet@gmail.com>
2020-05-28 20:45:20 -0300

    feat: pacote de calendário adicionado e configuração do locale para português

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Calendar, LocaleConfig, defaultLocale } from 'react-native-calendars';
+
+LocaleConfig.locales['pt-br'] = {
+  monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+  monthNamesShort: ['Jan','Fev','Mar','Abr','Maio','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+  dayNames: ['Domingo','Segunda-feira','Terça-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sábado'],
+  dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb'],
+  today: 'Hoje'
+};
+
+LocaleConfig.defaultLocale = 'pt-br';
 
 export default function App() {
+  const currentDate = new Date();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Calendar
+        style={{width:"90%"}}
+        // Initially visible month. Default = Date()
+        current={currentDate}
+      />
     </View>
   );
 }
@@ -14,6 +30,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 48,
   },
-});
+})
